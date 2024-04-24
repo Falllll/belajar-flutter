dd3d2803967cac381205b4280416ee1af87a8261
Falllll <falldyshinoda@gmail.com>
2024-04-24 10:57:59 +0700

    Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Latihan Column dan Row"),
+        appBar: AppBar(
+          title: Text("Container"),
+        ),
+        body: Container(
+          color: Colors.red,
+          margin: EdgeInsets.fromLTRB(10, 25, 30, 15),
+          padding: EdgeInsets.all(10),
+          child: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[Colors.amber, Colors.blue])),
           ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("Text 1"),
-              Text("Text 2"),
-              Text("Text 3"),
-              Row(
-                children: <Widget>[
-                  Text("Text 4"),
-                  Text("Text 5"),
-                  Text("Text 6")
-                ],
-              )
-            ],
-          )),
+        ),
+      ),
     );
   }
 }
