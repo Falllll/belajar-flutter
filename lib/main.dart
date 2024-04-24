58634a03b8cf85f74830024390fadbd9382056a2
Falllll <falldyshinoda@gmail.com>
2024-04-24 10:14:55 +0700

    Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,20 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Hello world"),
         ),
-        body: Center(child: Text("Hello world")),
+        body: Center(
+            child: Container(
+                color: Colors.amber,
+                width: 150,
+                height: 50,
+                child: Text(
+                  "Hello world",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ))),
       ),
     );
   }
