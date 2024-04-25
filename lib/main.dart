f2869c115bc909429191a7807f3aca2c667f8d78
Falllll <falldyshinoda@gmail.com>
2024-04-25 15:46:44 +0700

    TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -4,36 +4,27 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String message = "Tekan dong";
-
-  void pressed() {
-    setState(() {
-      message = "UwU";
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Anonymous Function Demo'),
+          title: Text("Latihan TextStyle"),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(message),
-              ElevatedButton(onPressed: pressed, child: Text("Tekan Saya"))
-            ],
+          child: Text(
+            "Ini adalah text",
+            style: TextStyle(
+                fontFamily: "Roboto",
+                fontSize: 30,
+                fontStyle: FontStyle.italic,
+                decoration: TextDecoration.lineThrough,
+                decorationColor: Colors.blueAccent,
+                decorationThickness: 1,
+                decorationStyle: TextDecorationStyle.dotted),
           ),
         ),
       ),
