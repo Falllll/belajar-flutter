a488e7a92d55cef4389771f698254dbe13707e75
Falllll <falldyshinoda@gmail.com>
2024-04-26 16:12:25 +0700

    Belajar list view

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,48 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> widgets = [];
+  int counter = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan TextStyle"),
+          title: Text("Latihan List"),
         ),
-        body: Center(
-          child: Text(
-            "Ini adalah text",
-            style: TextStyle(
-                fontFamily: "Roboto",
-                fontSize: 30,
-                fontStyle: FontStyle.italic,
-                decoration: TextDecoration.lineThrough,
-                decorationColor: Colors.blueAccent,
-                decorationThickness: 1,
-                decorationStyle: TextDecorationStyle.dotted),
-          ),
+        body: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    widgets.add(Text("Data ke-" + counter.toString()));
+                    counter++;
+                  });
+                }, child: Text("Tambah Data")),
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    widgets.removeLast();
+                    counter--;
+                  });
+                }, child: Text("Hapus Data"))
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: widgets,
+            )
+          ],
         ),
       ),
     );
