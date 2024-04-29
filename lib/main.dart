d2b1bf88dc2bb521018f4a607b86f02a13efd818
Falllll <falldyshinoda@gmail.com>
2024-04-29 11:21:55 +0700

    Flexible Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,46 @@
 import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Random random = Random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Latihan Animated Container"),),
-        body: Center(
-          child: GestureDetector(
-            onTap: (){
-              setState(() {
-                
-              });
-            },
-            child: AnimatedContainer(
-            color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
-            duration: Duration(seconds: 1),
-            width: 50.0 + random.nextInt(251),
-            height: 50.0 + random.nextInt(251),
-          ),
-        ),
+        appBar: AppBar(title: Text("Flexible Widget"),),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+                flex: 1,
+                child: Row(
+                  children: <Widget>[
+                    Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.red,)),
+                    Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.purple,)),
+                    Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.amber,)),
+                  ]
+                ),
+            ),
+            Flexible(
+                flex: 2,
+                child: Container(color: Colors.green,)),
+            Flexible(
+                flex: 1,
+                child: Container(color: Colors.blue,))
+          ],
         ),
       ),
     );
   }
 }
-
